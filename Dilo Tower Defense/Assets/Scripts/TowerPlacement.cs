555a22dd2b714389c010ed3ad253afc0005a9deb
sandi-hermawan <sandi.tugas01@gmail.com>
2021-09-19 20:42:33 +0700

    tugas keempat

---
+++ Dilo Tower Defense/Assets/Scripts/TowerPlacement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+    private Tower _placedTower;
+    private Tower tower;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_placedTower != null)
+        {
+            return;
+
+        }
+        Tower tower = collision.GetComponent<Tower>();
+
+        if (tower != null)
+        {
+            tower.SetPlacePosition(transform.position);
+            _placedTower = tower;
+            LevelManager.Instance.BuyTower(tower);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+
+    {
+
+        if (_placedTower == null)
+
+        {
+
+            return;
+
+        }
+
+        
+        _placedTower.SetPlacePosition(null);
+
+        _placedTower = null;
+
+    }
+}
